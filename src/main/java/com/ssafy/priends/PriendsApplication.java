4fd12e956e35bbff37b0d36b8a9969771445e79c
tlsehdrms124 <tlsehdrms124@naver.com>
2023-10-31 13:42:40 +0900

    초기 세팅 완료

---
+++ src/main/java/com/ssafy/priends/PriendsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.priends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriendsApplication.class, args);
+	}
+
+}
