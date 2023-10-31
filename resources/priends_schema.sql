2a9dbf511d7195c264e84f79a403bb06e62eed68
hggew <hggew@naver.com>
2023-10-31 16:29:35 +0900

    docs: db 수정

--- resources/priends_schema.sql
+++ resources/priends_schema.sql
@@ -3,58 +3,9 @@
 -- Host: 127.0.0.1    Database: priends
 -- ------------------------------------------------------
 -- Server version	8.0.34
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `boards`
---
-
-DROP TABLE IF EXISTS `boards`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `boards` (
-  `id` bigint NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `content` varchar(10000) NOT NULL,
-  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP,
-  `isdeleted` tinyint NOT NULL DEFAULT '0',
-  `category` enum('NOTICE','FREE','PATH') NOT NULL,
-  `userid` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `boards_to_members_id_fk_idx` (`userid`),
-  CONSTRAINT `boards_to_members_id_fk` FOREIGN KEY (`userid`) REFERENCES `members` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `comments`
---
-
-DROP TABLE IF EXISTS `comments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `comments` (
-  `id` bigint NOT NULL,
-  `content` varchar(1000) NOT NULL,
-  `isdeleted` tinyint(1) DEFAULT '0',
-  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
-  `boardid` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `comments_to_boards_id_fk_idx` (`boardid`),
-  CONSTRAINT `comments_to_boards_id_fk` FOREIGN KEY (`boardid`) REFERENCES `boards` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+DROP DATABASE IF EXISTS `priends`;
+create database priends;
+use priends;
 
 --
 -- Table structure for table `members`
@@ -76,6 +27,50 @@ CREATE TABLE `members` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `boards`
+--
+
+DROP TABLE IF EXISTS `boards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `boards` (
+  `id` bigint NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `content` varchar(10000) NOT NULL,
+  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `isDeleted` tinyint NOT NULL DEFAULT '0',
+  `category` enum('NOTICE','FREE','PATH') NOT NULL,
+  `user_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `boards_to_members_id_fk_idx` (`user_id`),
+  CONSTRAINT `boards_to_members_id_fk` FOREIGN KEY (`user_id`) REFERENCES `members` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `id` bigint NOT NULL,
+  `content` varchar(1000) NOT NULL,
+  `isDeleted` tinyint(1) DEFAULT '0',
+  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `board_id` bigint NOT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comments_to_boards_id_fk_idx` (`board_id`),
+  KEY `comments_to_members_id_fk_idx` (`user_id`),
+  CONSTRAINT `comments_to_boards_id_fk` FOREIGN KEY (`board_id`) REFERENCES `boards` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `comments_to_members_id_fk` FOREIGN KEY (`user_id`) REFERENCES `members` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `path`
 --
 
@@ -85,19 +80,11 @@ DROP TABLE IF EXISTS `path`;
 CREATE TABLE `path` (
   `id` bigint NOT NULL,
   `content` varchar(1000) NOT NULL,
-  `isdeleted` tinyint(1) DEFAULT '0',
+  `isEnd` tinyint(1) DEFAULT '0',
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `start_date` datetime DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2023-10-31 16:10:05
