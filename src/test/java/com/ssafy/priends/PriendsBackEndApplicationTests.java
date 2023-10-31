3b26a75b9148a7e71ac99e92152daed0d3447780
tlsehdrms124 <tlsehdrms124@naver.com>
2023-10-31 14:38:08 +0900

    feat: Spring boot 초기 세팅 완료

---
+++ src/test/java/com/ssafy/priends/PriendsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.priends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriendsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
