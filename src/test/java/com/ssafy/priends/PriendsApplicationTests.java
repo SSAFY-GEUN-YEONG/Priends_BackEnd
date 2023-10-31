4773fe766a3aa34296036d675e99aa5a4ee7e5c0
hggew <hggew@naver.com>
2023-10-31 14:09:40 +0900

    init commit

---
+++ src/test/java/com/ssafy/priends/PriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.priends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
