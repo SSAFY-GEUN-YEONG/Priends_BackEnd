ff85046c42be4fbfa40c5f048b9cb6b3544be314
hggew <hggew@naver.com>
2023-11-02 21:33:59 +0900

    fix: DB column 수정, 추가

--- src/main/java/com/ssafy/priends/domain/board/dto/BoardMemberDto.java
+++ src/main/java/com/ssafy/priends/domain/board/dto/BoardMemberDto.java
@@ -11,7 +11,7 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor	// 필드 전부 생성자
 @NoArgsConstructor	// 기본 생성자
-public class BoardDto {
+public class BoardMemberDto {
 	
 	private Long id;
 	private String title;
@@ -23,4 +23,7 @@ public class BoardDto {
 	private boolean isdeleted;
 	private String category;
 	private Long member_id;
+	
+	private String member_email;
+	private String member_name;
 }
