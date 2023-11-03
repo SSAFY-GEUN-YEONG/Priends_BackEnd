95a20d6e197b589ebfd47033cd4a86041c3148be
동긔 <tlsehdrms124@naver.com>
2023-11-03 15:38:59 +0900

    chore: 게시판 requestbody 공통부분 설정

--- src/main/java/com/ssafy/priends/domain/board/controller/BoardController.java
+++ src/main/java/com/ssafy/priends/domain/board/controller/BoardController.java
@@ -1,10 +1,10 @@
 package com.ssafy.priends.domain.board.controller;
 
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.http.HttpSession;
-
+import com.ssafy.priends.global.common.dto.Message;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,63 +14,60 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.ssafy.priends.domain.board.dto.BoardDto;
 import com.ssafy.priends.domain.board.dto.BoardMemberDto;
 import com.ssafy.priends.domain.board.service.BoardService;
-import com.ssafy.priends.domain.member.dto.MemberDto;
 
-import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/board")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class BoardController {
 	
-	private BoardService boardService;
-	
+	private final BoardService boardService;
+
 	// 멤버에서 토큰 보내주면 받아와서 boardDto에 넘겨줘야함
 	@PostMapping("/write")
-	public ResponseEntity<?> writePost( @RequestBody BoardDto boardDto ) throws Exception {
+	public ResponseEntity<Message<Void>> writePost( @RequestBody BoardDto boardDto ) throws Exception {
 //		MemberDto member = (MemberDto) session.getAttribute("userinfo");
 //		board.setUser_id(member.getId());
 		
 		boardService.writePost(boardDto);
 		
-		return ResponseEntity.ok().body(0); //0:성공, 1:실패
+		return ResponseEntity.ok().body(Message.success()); //0:성공, 1:실패
 	}
  
 	// category에 따라서 글 가져오기 -> category : 'NOTICE','FREE','PATH'
 	@GetMapping("/list")
-	public ResponseEntity<?> listPost(@RequestParam String category) throws Exception {
+	public ResponseEntity<Message<List<BoardMemberDto>>> listPost(@RequestParam String category) throws Exception {
 		List<BoardMemberDto> list = boardService.listPost(category);
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(Message.success(list));
 	}
 
 	@GetMapping("/view")
-	public ResponseEntity<?> getPost(@RequestParam("id") long id ) throws Exception {
+	public ResponseEntity<Message<BoardMemberDto>> getPost(@RequestParam("id") long id ) throws Exception {
 		System.out.println("id=" + id);
 		boardService.updateHit(id);
 		BoardMemberDto board = boardService.getPost(id);
-		return ResponseEntity.ok().body(board);
+		return ResponseEntity.ok().body(Message.success(board));
 	}
 
 	@GetMapping("/modify")
-	public ResponseEntity<?> modifyPost(@RequestParam("id") int id ) throws Exception {
+	public ResponseEntity<Message<BoardMemberDto>> modifyPost(@RequestParam("id") int id ) throws Exception {
 		BoardMemberDto board = boardService.getPost(id);
-		return ResponseEntity.ok().body(board);
+		return ResponseEntity.ok().body(Message.success(board));
 	}
 
 	@PostMapping("/modify")
-	public ResponseEntity<?> modifyPost(@RequestBody BoardDto board) throws Exception {
+	public ResponseEntity<Message<Void>> modifyPost(@RequestBody BoardDto board) throws Exception {
 		boardService.modifyPost(board);
-		return ResponseEntity.ok().body(0);
+		return ResponseEntity.ok().body(Message.success());
 	}
 
 	@GetMapping("/delete")
-	public ResponseEntity<?> deletePost(@RequestParam("id") long id) throws Exception {
+	public ResponseEntity<Message<Void>> deletePost(@RequestParam("id") Long id) throws Exception {
 		boardService.deletePost(id);
-		return ResponseEntity.ok().body(0);
+		return ResponseEntity.ok().body(Message.success());
 	}
 }
