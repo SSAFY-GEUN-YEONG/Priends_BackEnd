908de9191ae3e1017968aac36043398fd5b3e35f
동긔 <tlsehdrms124@naver.com>
2023-11-03 14:41:56 +0900

    feat: 회원파트 개발 완료

---
+++ src/main/java/com/ssafy/priends/domain/member/dto/MemberLoginRequestDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.priends.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberLoginRequestDto {
+    String email;
+    String password;
+}
