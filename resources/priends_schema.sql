2bd42335022a7a8513d3926e6bbebd257d1a9f55
hggew <hggew@naver.com>
2023-11-20 09:25:57 +0900

    feat: DB수정

--- resources/priends_schema.sql
+++ resources/priends_schema.sql
@@ -2,14 +2,11 @@
 --
 -- Host: 127.0.0.1    Database: priends
 -- ------------------------------------------------------
--- Server version	8.0.34
+-- Server version   8.0.34
 DROP DATABASE IF EXISTS `priends`;
 create database priends;
 use priends;
-
---
--- Table structure for table `members`
---
+ 
 
 DROP TABLE IF EXISTS `members`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -18,11 +15,12 @@ CREATE TABLE `members` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `password` varchar(500) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `name` varchar(10) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
-  `alarm` tinyint(1) DEFAULT '0',
+  `nickname` varchar(10) NOT NULL,
+  `image` varchar(1000) NOT NULL,
+  `status` tinyint NOT NULL DEFAULT '0',
+  `alarm` tinyint NOT NULL DEFAULT '0',
   `role` enum('ADMIN','MANAGER','USER') DEFAULT NULL,
-  `social` tinyint(1) DEFAULT '0',
+  `social` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -43,8 +41,8 @@ CREATE TABLE `boards` (
   `like` INT NOT NULL DEFAULT 0,
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP,
-  `is_deleted` tinyint NOT NULL DEFAULT 0,
-  `category` enum('NOTICE','FREE','PATH') NOT NULL,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  `category` enum('NOTICE','FREE','QNA') NOT NULL,
   `member_id` bigint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `boards_to_members_id_fk_idx` (`member_id`),
@@ -62,7 +60,7 @@ DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `content` varchar(1000) NOT NULL,
-  `is_deleted` tinyint(1) DEFAULT 0,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
   `board_id` bigint NOT NULL,
   `member_id` bigint DEFAULT NULL,
@@ -86,7 +84,8 @@ CREATE TABLE `path` (
   `title` varchar(50) NOT NULL,
   `content` varchar(1000) NOT NULL,
   `hit` INT NOT NULL DEFAULT 0,
-  `is_end` tinyint(1) DEFAULT 0,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  `is_end` tinyint NOT NULL DEFAULT '0',
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
   `start_date` datetime DEFAULT NULL,
   `end_date` datetime DEFAULT NULL,
@@ -108,16 +107,35 @@ CREATE TABLE `priends`.`path_details` (
   `content_id` INT NOT NULL,
   `path_id` BIGINT NOT NULL,
   PRIMARY KEY (`id`), 
-  CONSTRAINT `path_details_to_attraction_info_content_id`
+  CONSTRAINT `path_details_to_attraction_info_content_id_fk`
     FOREIGN KEY (`content_id`)
     REFERENCES `priends`.`attraction_info` (`content_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `path_details_to_path_path_id`
+  CONSTRAINT `path_details_to_path_path_id_fk`
     FOREIGN KEY (`path_id`)
     REFERENCES `priends`.`path` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+    
+DROP TABLE IF EXISTS `alarm`;
+CREATE TABLE `alarm` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `createdAt` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `content` varchar(80) NOT NULL,
+  `read` tinyint NOT NULL DEFAULT '0',
+  `from_member_id` BIGINT NOT NULL,
+  `to_member_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`), 
+  CONSTRAINT `alarm_to_members_from_id_fk`
+    FOREIGN KEY (`from_member_id`)
+    REFERENCES `priends`.`members` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `alarm_to_members_to_id_fk`
+    FOREIGN KEY (`to_member_id`)
+    REFERENCES `priends`.`members` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
 -- Dump completed on 2023-10-31 16:10:05 
-  
